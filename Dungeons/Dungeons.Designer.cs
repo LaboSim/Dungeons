f2749b91b2f3a9e691813e3f94081203d1236060
Szymon Cierzniak <scierzniak96@gmail.com>
2019-03-21 10:22:54 +0100

    Load of game board

---
+++ Dungeons/Dungeons.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Dungeons
+{
+    partial class Dungeons
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dungeons));
+            this.SuspendLayout();
+            // 
+            // Dungeons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Dungeons";
+            this.Text = "Dungeons";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
